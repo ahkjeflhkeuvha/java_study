dbd4acfc6513b4888d5900cab5289930f0f94cd7
kangin wife <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-04-04 09:48:41 +0900

    나도 이제부터 자바 개발자이다

---
+++ Test_01.java
@@ -0,0 +1,16 @@
+public class Test_01 {
+
+	public static void main(String args[]) {
+		System.out.println("hello Java~");
+		System.out.println("나도 이제부터 자바 개발자이다");
+	
+	}
+
+} 
+
+실행하는 법
+1. 자바 스터디 주소 복사
+2. cmd 파일에 적기
+3. javac Test_01.java
+(4. dir Test_01.java)
+5. java Test_01
