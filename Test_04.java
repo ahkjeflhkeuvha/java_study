638fef36932086852889e208820a79fcd02cdd5f
kangin wife <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-04-04 09:53:43 +0900

    int, string 변수 선언 후 출

---
+++ Test_04.java
@@ -0,0 +1,33 @@
+public class Test_04{
+	public static void main(String args[]){
+	
+		String str1 = "미스터트롯2";
+		String str2 = "김용필";
+		System.out.print(str1);
+		System.out.print(str2);
+
+		int a; //변수 선언
+		a = 48; //값 초기화
+		System.out.println(str1+"에 출연한"+" "+str2+"은"+" "+a+"세입니다.");
+		System.out.println(str1+"에 출연한 "+str2+"은 "+a+"세입니다.");
+
+		System.out.print(str1);
+		System.out.print("에 출연한 ");
+		System.out.print(str2);
+		System.out.print("은 ");
+		System.out.print(a);
+		System.out.println("세입니다.");
+
+		a = 49;
+		System.out.println("내년에 "+str2+"은 "+a+"세입니다.");
+		System.out.print("내년에 ");
+		System.out.print(str2);
+		System.out.print("은 ");
+		System.out.print(a);
+		System.out.println("세입니다.");
+		System.out.print(" 					즐");
+		
+		
+
+	}
+}
