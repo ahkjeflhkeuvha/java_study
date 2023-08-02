2df75c0ff8f803204e9f24467e7b9f134aff7ee3
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-08-02 20:11:16 +0900

    여러가지 for문과 관련된 예제

---
+++ For_Study_01.java
@@ -0,0 +1,65 @@
+class For_Study_01 
+{
+	public static void main(String[] args) 
+	{
+//		int a;
+//		int sum = 0;
+//		for(a = 1; a<=10; a++){ // 시작점과 = 여부 확인하고 시작하기
+//			System.out.println(a);
+//			sum += a;
+//		}
+//		System.out.println();
+//		System.out.println("a = "+a);
+//		System.out.println("합계 = "+ sum);
+
+//		for(int i = 65; i<=90; i++){
+//			System.out.print((char)i+" ");
+//			// 0 = 48, A = 65, a = 97
+//		}
+
+//		for(int i = 'A'; i<='Z'; i++){
+//			System.out.print((char)i + " ");
+//		}
+
+		
+		// 다중 for문
+
+//		for(int m = 1; m<=3; m++){
+//			System.out.println("m = " + m);
+//			for(int n = 1; n<=4; n++){
+//				System.out.print("n = " + n+"\t");
+//			}
+//			System.out.println();
+//			System.out.println("----------------------------");
+//		}
+//
+//		// m과 n값이 출력되지 않음
+
+//		for(char i = 65; i<=90; i++){
+//			System.out.print(i + " : ");
+//			for(char j = i; j<='Z'; j++){
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+
+		// continue, break;
+
+//		for(int i = 1; i<=10; i++){
+//			if(i == 7) continue;
+//			System.out.println(i);
+//
+//		}
+
+//		int kk = 0;
+//
+//		for(;;){ // 아무것도 안 넣으면 무한루프~
+//			if(kk == 5) break; // 이렇게 해도 무한루프~~
+//			
+//			System.out.println("//^ _ ^//");
+//			kk++;
+//		}
+
+	}
+}
