213574d73638d1e53a6cae89dad7c5716692d8ef
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-08-08 09:14:06 +0900

    정수를 입력 받아 각 자리의 합 구하기
    while문 -> 몫과 나머지 사용
    실수 했던 이유 = num을 10으로 나누고 한 번 더  num을 10으로 나눈 값이 0이면 break하라고 함 ^-^ 해결 완료~(´▽`ʃ♡ƪ)

--- For_Study_03.java
+++ For_Study_03.java
@@ -22,17 +22,42 @@ class For_Study_03
 //		System.out.println("경우의 수 : "+count);
 		
 
+
+
+
 		// 문제 2.  문자형 숫자를 입력받아 각 자리의 합을 구하시오. 
 
-		Scanner sc = new Scanner(System.in);
-		String num = sc.next();
-		int sum = 0;
-		int num_int = Integer.parseInt(num);
+//		Scanner sc = new Scanner(System.in);
+//		String num = sc.next();
+//		int sum_1 = 0;
+//		int sum_2 = 0;
+//		int num_int = Integer.parseInt(num);
+//		
+//		for(int i = 0; i<num.length(); i++){
+//			sum_1 += num_int%10;
+//			num_int/=10;
+//		}
+//
+//		for(int i = 0; i<num.length(); i++){
+//			sum_2 += num.charAt(i)-'0';
+//		}
+//		System.out.println("Integer.parseInt 사용 값 : "+sum_1+"\ncharAt() 사용 값 : "+sum_2);
+
+
+
 		
-		for(int i = 0; i<num.length(); i++){
-			sum += num_int%10;
-			num_int/=10;
-		}
-		System.out.println(sum);
+		// 문제 3.  정수를 입력받아 각 자리의 합을 구하시오.
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int sum = 0;
+
+		while(true){
+			sum += num%10;
+			num/=10;
+			if(num== 0) break;
+ 		}
+
+		System.out.println();
+		System.out.println("각 자리 수의 총 합 : "+sum);
 	}
 }
