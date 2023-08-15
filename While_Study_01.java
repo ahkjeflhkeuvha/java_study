ba3a375a35a2c4804e9c0dbb7190dc180d11f0a3
지은입니다 <128341152+ahkjeflhkeuvha@users.noreply.github.com>
2023-08-16 08:52:52 +0900

    while문 예제

---
+++ While_Study_01.java
@@ -0,0 +1,51 @@
+package java_project;
+
+public class While_Study_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//			int i = 1;
+//			while(i<=5) {
+//				System.out.println(i);
+//				i++;
+//			}
+//			System.out.println();
+//			System.out.println("반복문 탈출 후  i 값은 : "+ i);
+		
+			// QUIZ - while문을 이용해 1에서 10 사이의 홀수만 출력하기
+//			int i = 1;
+//			while(i<=10) {
+//				if(i%2 == 1) System.out.println(i);
+//				i++;
+//			}
+//			
+//			System.out.println();
+		
+//			
+//			int j = 1;
+//			while(j<=10) {
+//				System.out.println(j);
+//				j+=2;
+//			}
+		
+//		int i = 0;
+//		while(true) {
+//			i++;
+//			if(i==10) break;		
+//			if(i%2==0) continue;
+//			System.out.println(i);
+//		}
+		
+		int i = 10;
+		do {
+			System.out.println(i);
+		}
+		while(i<=5);
+		// 조건식이 false여도 한 번은 실행
+		
+		
+		
+	}
+
+}
